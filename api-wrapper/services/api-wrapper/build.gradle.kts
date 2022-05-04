a4f0f7b1fe3a223c6fdeb3df2588b008b7021107
Tuncay Tunc (ZF Friedrichshafen AG) <100704677+tuncaytunc-zf@users.noreply.github.com>
2022-05-04 14:04:04 +0200

    Add API-Documentation

--- api-wrapper/services/api-wrapper/build.gradle.kts
+++ api-wrapper/services/api-wrapper/build.gradle.kts
@@ -2,11 +2,15 @@ plugins {
     `java-library`
     id("application")
     id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("com.rameshkp.openapi-merger-gradle-plugin") version "1.0.4"
+    id("io.swagger.core.v3.swagger-gradle-plugin") version "2.2.0"
 }
 
 val javaVersion = 11
 val edcGroup = "org.eclipse.dataspaceconnector"
 val edcVersion = "0.0.1-SNAPSHOT"
+val rsApi = "3.0.0"
+val swaggerJaxrs2Version = "2.1.11"
 
 dependencies {
     api("$edcGroup:core-boot:$edcVersion")
@@ -48,3 +52,57 @@ repositories {
         url = uri("https://maven.iais.fraunhofer.de/artifactory/eis-ids-public/")
     }
 }
+
+buildscript {
+    dependencies {
+        classpath("io.swagger.core.v3:swagger-gradle-plugin:2.1.12")
+    }
+}
+
+pluginManager.withPlugin("io.swagger.core.v3.swagger-gradle-plugin") {
+
+    dependencies {
+        // this is used to scan the classpath and generate an openapi yaml file
+        implementation("io.swagger.core.v3:swagger-jaxrs2-jakarta:${swaggerJaxrs2Version}")
+        implementation("jakarta.ws.rs:jakarta.ws.rs-api:${rsApi}")
+    }
+// this is used to scan the classpath and generate an openapi yaml file
+    tasks.withType<io.swagger.v3.plugins.gradle.tasks.ResolveTask> {
+        outputFileName = project.name
+        outputFormat = io.swagger.v3.plugins.gradle.tasks.ResolveTask.Format.YAML
+        prettyPrint = true
+        classpath = java.sourceSets["main"].runtimeClasspath
+        buildClasspath = classpath
+        resourcePackages = setOf("org.eclipse.dataspaceconnector")
+        outputDir = file("${rootProject.projectDir.path}/resources/openapi/yaml")
+    }
+    configurations {
+        all {
+            exclude(group = "com.fasterxml.jackson.jaxrs", module = "jackson-jaxrs-json-provider")
+        }
+    }
+}
+
+openApiMerger {
+    val yamlDirectory = file("${rootProject.projectDir.path}/resources/openapi/yaml")
+
+    inputDirectory.set(yamlDirectory)
+    output {
+        directory.set(file("${rootProject.projectDir.path}/resources/openapi/"))
+        fileName.set("openapi")
+        fileExtension.set("yaml")
+    }
+    openApi {
+        openApiVersion.set("3.0.1")
+        info {
+            title.set("API-Wrapper REST API")
+            description.set("All files merged by open api merger")
+            version.set("1.0.0-SNAPSHOT")
+            license {
+                name.set("Apache License v2.0")
+                url.set("http://apache.org/v2")
+            }
+        }
+    }
+}
+
