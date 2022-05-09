3e78f50cbc91b8cf2fcc99fb1f217991ca9a52f9
Diego Gomez <d.gomez@cluetec.de>
2022-05-09 12:02:32 +0200

    Create ApiWrapper Model Tests from classes ContractOfferDescription, NegotiationId, NegotiationInitiateRequestDto and TransferId

---
+++ api-wrapper/services/api-wrapper/src/test/java/org/eclipse/dataspaceconnector/apiwrapper/connector/sdk/model/TransferIdTest.java
@@ -0,0 +1,33 @@
+package org.eclipse.dataspaceconnector.apiwrapper.connector.sdk.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransferIdTest {
+
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    void verifySerialization() throws JsonProcessingException {
+
+        TransferId transferId = TransferId.Builder.newInstance().id("transferId").build();
+
+        var str = objectMapper.writeValueAsString(transferId);
+
+        assertThat(str).isNotNull();
+
+        var deserialized = objectMapper.readValue(str, TransferId.class);
+        assertThat(deserialized).usingRecursiveComparison().isEqualTo(transferId);
+    }
+
+}
