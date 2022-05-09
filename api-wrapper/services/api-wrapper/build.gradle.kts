18eb421d7c95384a28bd0a1a25f27e84b9d8dd9c
Diego Gomez <d.gomez@cluetec.de>
2022-05-09 13:54:09 +0200

    Change test dependencies from implementation to testImplementation

--- api-wrapper/services/api-wrapper/build.gradle.kts
+++ api-wrapper/services/api-wrapper/build.gradle.kts
@@ -26,9 +26,9 @@ dependencies {
     implementation("jakarta.ws.rs:jakarta.ws.rs-api:3.1.0")
     implementation("com.auth0:java-jwt:3.19.2")
 
-    implementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    implementation("org.assertj:assertj-core:3.21.0")
-    implementation("org.mockito:mockito-inline:3.6.28")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.assertj:assertj-core:3.21.0")
+    testImplementation("org.mockito:mockito-inline:3.6.28")
 }
 
 java {
