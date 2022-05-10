0735db41b8a519b6419e2e038e63c8f644407318
Diego Alejandro Gómez Pardo (ZF Friedrichshafen AG) <97615391+diegogomez-zf@users.noreply.github.com>
2022-05-10 15:29:25 +0200

    Create Unit Tests for Api Wrapper (#66)
    Co-authored-by: Florian Rusch (ZF Friedrichshafen AG) <florian.rusch.external@zf.com>

---
+++ api-wrapper/services/api-wrapper/src/test/java/org/eclipse/dataspaceconnector/apiwrapper/config/ApiWrapperConfigTest.java
@@ -0,0 +1,29 @@
+package org.eclipse.dataspaceconnector.apiwrapper.config;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class ApiWrapperConfigTest {
+
+    @Test
+    void testBuild() {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("userId", "userPwd");
+
+        ApiWrapperConfig apiWrapperTest = ApiWrapperConfig.Builder.newInstance()
+                .consumerEdcDataManagementUrl("urlConsumer")
+                .consumerEdcApiKeyValue("apiValue")
+                .basicAuthUsers(map).build();
+
+        assertAll(() -> assertThat(apiWrapperTest.getConsumerEdcDataManagementUrl()).isEqualTo("urlConsumer"),
+                () -> assertThat(apiWrapperTest.getConsumerEdcApiKeyValue()).isEqualTo("apiValue"),
+                () -> assertThat(apiWrapperTest.getBasicAuthUsers().get("userId")).isEqualTo("userPwd"),
+                () -> assertThat(apiWrapperTest.getConsumerEdcApiKeyName()).isEqualTo("X-Api-Key"));
+    }
+}
---
+++ api-wrapper/services/api-wrapper/src/test/java/org/eclipse/dataspaceconnector/apiwrapper/connector/sdk/model/NegotiationIdTest.java
@@ -0,0 +1,32 @@
+package org.eclipse.dataspaceconnector.apiwrapper.connector.sdk.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NegotiationIdTest {
+
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    void verifySerialization() throws JsonProcessingException {
+        NegotiationId negotiationId = NegotiationId.Builder.newInstance().id("negotiationId").build();
+
+        var str = objectMapper.writeValueAsString(negotiationId);
+
+        assertThat(str).isNotNull();
+
+        var deserialized = objectMapper.readValue(str, NegotiationId.class);
+        assertThat(deserialized).usingRecursiveComparison().isEqualTo(negotiationId);
+    }
+
+}
