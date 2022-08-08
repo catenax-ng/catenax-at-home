88cfe87c043e3f70b04019ab7e6ebbd518802135
Florian Rusch (ZF Friedrichshafen AG) <florian.rusch.external@zf.com>
2022-08-08 10:54:54 +0200

    Update EDC setup regarding 0.1.0 and product-edc images

--- api-wrapper/0-init-provider.sh
+++ api-wrapper/0-init-provider.sh
@@ -9,11 +9,10 @@ set -o nounset
 curl -X POST -H 'Content-Type: text/plain' --data "test" http://localhost:8194/data/asset-1
 
 # Create a asset
-curl -X POST -H 'Content-Type: application/json' --data "@resources/asset.json" http://localhost:8187/api/v1/data/assets
+curl -X POST -H 'Content-Type: application/json' -H 'X-API-Key: 123456' --data "@resources/asset.json" http://localhost:8187/api/v1/data/assets
 
 # Create a general policy
-curl -X POST -H 'Content-Type: application/json' --data "@resources/policy.json" http://localhost:8187/api/v1/data/policies
+curl -X POST -H 'Content-Type: application/json' -H 'X-API-Key: 123456' --data "@resources/policy.json" http://localhost:8187/api/v1/data/policydefinitions
 
 # Create a contract definition
-curl -X POST -H 'Content-Type: application/json' --data "@resources/contractdefinition.json" http://localhost:8187/api/v1/data/contractdefinitions
-
+curl -X POST -H 'Content-Type: application/json' -H 'X-API-Key: 123456' --data "@resources/contractdefinition.json" http://localhost:8187/api/v1/data/contractdefinitions
--- api-wrapper/README.md
+++ api-wrapper/README.md
@@ -1,3 +1,27 @@
 # Local build guide
 
-! TO BE DEFINED !
+## 1. Setup vault
+
+```shell
+docker-compose -f ./docker-compose.vault.yml --build -d 
+```
+
+## 2. Setup EDCs
+
+Wait a view seconds until the vault was initialized.
+
+```shell
+docker-compose up -d
+```
+
+## See logs
+
+```shell
+docker-compose -f ./docker-compose.vault.yml -f ./docker-compose.yml logs -f
+```
+
+## Destroy everything
+
+```shell
+docker-compose -f ./docker-compose.vault.yml -f ./docker-compose.yml down
+```
