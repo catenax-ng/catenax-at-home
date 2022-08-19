0bb0ea3f2bc970b221d3ed0889db4aca637f53c8
Florian Rusch (ZF Friedrichshafen AG) <florian.rusch.external@zf.com>
2022-08-19 06:50:14 +0200

    Update dependencies

--- api-wrapper/services/api-wrapper/build.gradle.kts
+++ api-wrapper/services/api-wrapper/build.gradle.kts
@@ -8,8 +8,6 @@ plugins {
 val javaVersion = 11
 val edcGroup = "org.eclipse.dataspaceconnector"
 val edcVersion = "0.0.1-milestone-5"
-val rsApi = "3.0.0"
-val swaggerJaxrs2Version = "2.2.0"
 
 dependencies {
     implementation("$edcGroup:core-boot:$edcVersion")
@@ -24,12 +22,12 @@ dependencies {
     implementation("$edcGroup:auth-spi:$edcVersion")
 
     implementation("jakarta.ws.rs:jakarta.ws.rs-api:3.1.0")
-    implementation("com.auth0:java-jwt:3.19.2")
-    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.auth0:java-jwt:4.0.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testImplementation("org.assertj:assertj-core:3.22.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testImplementation("org.assertj:assertj-core:3.23.1")
     testImplementation("org.mockito:mockito-inline:4.7.0")
 }
 
@@ -63,15 +61,14 @@ repositories {
 
 buildscript {
     dependencies {
-        classpath("io.swagger.core.v3:swagger-gradle-plugin:2.1.12")
+        classpath("io.swagger.core.v3:swagger-gradle-plugin:2.2.0")
     }
 }
 
 pluginManager.withPlugin("io.swagger.core.v3.swagger-gradle-plugin") {
-
     dependencies {
-        implementation("io.swagger.core.v3:swagger-jaxrs2-jakarta:${swaggerJaxrs2Version}")
-        implementation("jakarta.ws.rs:jakarta.ws.rs-api:${rsApi}")
+        implementation("io.swagger.core.v3:swagger-jaxrs2-jakarta:2.2.2")
+        implementation("jakarta.ws.rs:jakarta.ws.rs-api:3.0.0")
     }
 
     tasks.withType<io.swagger.v3.plugins.gradle.tasks.ResolveTask> {
